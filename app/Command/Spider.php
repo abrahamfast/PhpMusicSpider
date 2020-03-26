22993693a62dd82512343bc6249835fda001287c
abraham ghaemi <ig.ghaemi@gmail.com>
2020-03-27 00:31:02 +0430

    init project and skeleton

---
+++ app/Command/Spider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Command;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+
+class Spider extends Command
+{
+    protected static $defaultName = 'spider:get ';
+    protected function configure()
+    {
+      $this->setDescription('grep new url.')
+           ->setHelp('This command allows you grep new url...');
+      $this->addArgument('url', InputArgument::REQUIRED, 'The url of the site list.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        var_dump($input->getArgument('url'));
+
+        return 0;
+    }
+}
